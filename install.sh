5ff7839ea02a9f24f10e741c7f17f6bd8a7fd24d
Ben Hili <ben.hili1994@gmail.com>
2020-07-13 10:04:44 +1000

    feat: fix up options for osx

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
 ln -sf ~/dotfiles/nvim ~/.config/nvim
 ln -sf ~/dotfiles/emacs ~/.doom.d
-ln -sf ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+# ln -sf ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
