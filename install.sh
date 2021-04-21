08c976f748d224eb52c993cd901015f57bbabab0
Ben Hili <ben.hili1994@gmail.com>
2021-04-21 11:49:03 +1000

    Add proxy script and update vscode config

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
 ln -sf ~/dotfiles/nvim ~/.config/nvim
-ln -sf ~/dotfiles/emacs ~/.doom.d
+# ln -sf ~/dotfiles/emacs ~/.doom.d
 # ln -sf ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
 ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
