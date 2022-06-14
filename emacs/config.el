8ec4e1cae0355ba3b304a2a3eaf41109dd1233c6
Ben Hili <ben.hili1994@gmail.com>
2022-06-14 18:17:39 +1000

    Fix weirdness with rebase overwriting emacs config
    a bunch of old code weaseled it's way in so the diff
    will look rather strange just tried to revert it to its
    usual state

--- emacs/config.el
+++ emacs/config.el
@@ -1,37 +1,11 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
 (setq user-full-name "Ben Hili"
       user-mail-address "ben.hili1994@gmail.com")
 
-(setq doom-font (font-spec :family "IBM Plex Mono" :size 13))
-(setq doom-theme 'doom-city-lights)
-(setq lsp-log-io t)
-
-;; Escape vim mode sequence
-(setq-default evil-escape-key-sequence "fd")
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
+;; For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package' for configuring packages
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-
+(setq org-directory "~/org/")
 
 (setq-default evil-escape-key-sequence "fd")
 
