c44d5531d781e474ad3a6ca34132d3e5107db7c4
Ben Hili <ben.hili1994@gmail.com>
2022-06-14 17:34:45 +1000

    Add doom emacs config and gitignore

--- emacs/config.el
+++ emacs/config.el
@@ -24,64 +24,21 @@
 ;;
 ;; - `load!' for loading external *.el files relative to this one
 ;; - `use-package' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
 ;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
 ;;
-;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
 
-(require 'prettier-js)
-(setq js-indent-level 2)
-(setq-default typescript-indent-level 2)
+(setq-default evil-escape-key-sequence "fd")
 
-(add-hook 'js2-mode-hook  'prettier-js-mode)
-(add-hook 'typescript-mode-hook 'prettier-js-mode)
-(add-hook 'typescript-mode-hook 'prettier-js-mode)
+(setq doom-font (font-spec :family "Fira Code" :size 13)
+      doom-big-font (font-spec :family "Fira Code" :size 16))
 
-;; highlight src blocks in org mode
-(setq org-src-fontify-natively t)
-(setq org-directory (expand-file-name "~/Documents/org"))
+(set-face-bold-p 'bold nil)
 
-(setq company-idle-delay 0)
-(setq company-minimum-prefix-length 2)
-
-(defun prettier-format-php ()
-  "Format php files with prettier on save"
-  (when (eq major-mode 'php-mode)
-    (shell-command-to-string (format "prettier %s --write" buffer-file-name))))
-
-(add-hook 'after-save-hook #'prettier-format-php)
-
-;; Use prettier for tsx files
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
-(add-hook 'web-mode-hook
-          (lambda ()
-            (when (string-equal "tsx" (file-name-extension buffer-file-name))
-              (prettier-js-mode))))
-
-
-;; Ivy posframe
-(require 'ivy-posframe)
-(setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-window-center)))
-(ivy-posframe-mode 1)
-
-(add-hook 'org-mode-hook 'flyspell-mode)
-(add-hook 'terraform-mode-hook #'terraform-format-on-save-mode)
-
-;; phpactor completion
-(use-package php-mode
-  ;;
-  :hook ((php-mode . (lambda () (set (make-local-variable 'company-backends)
-       '(;; list of backends
-         company-phpactor
-         company-files
-         ))))))
+(setq ns-use-thin-smoothing t)
+(setq pixel-scroll-precision-mode t)
--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
-ln -sf ~/dotfiles/nvim ~/.config/nvim
-#  ln -sf ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
-ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -sf ~/dotfiles/emacs/* ~/.doom.d/
+
+# Neovim and VSCode configs (unused)
+# ln -sf ~/dotfiles/nvim ~/.config/nvim
+# ln -sf ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+# ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
